b1768370222a01005b0e64019118a93a1d811e30
AshankKumar <yohaw1@gmail.com>
2022-02-17 14:23:51 -0600

    Add files via upload

--- merge_bios_and_posts.py
+++ merge_bios_and_posts.py
@@ -1,16 +1,30 @@
+from numpy import column_stack
 import pandas as pd
 
 bios = pd.read_csv("bios.csv")
-posts = pd.read_csv("Posts_Info.csv")
+posts = pd.read_csv("posts_hundred_comments_only_twenty_twenty.csv")
 
-# rename column in posts so merge is easy
-posts = posts.rename(columns={"id": "link_id"})
+# only keep relevant columns from each df
+posts = posts[['Unnamed: 0', 'author', 'full_link', 'id', 'permalink', 'updated_utc', 'url']]
+bios = bios[['body', 'link_id']]
 
-# left merge on bios
-merged = pd.merge(bios, posts, how="left", on="link_id")
+# add suffix to columns
+posts.columns = [col_name + "_posts" for col_name in posts.columns]
+bios.columns = [col_name + "_bios" for col_name in bios.columns]
 
-# mask to remove comments that don't actually contain the bio
-mask = merged['body'].str.contains("The OP has not provided a Bio for their post.") == False
+# rename column for joining
+posts = posts.rename(columns={"id_posts": "link_id_posts"})
+
+# remove t3_ suffix from bios
+bios['link_id_bios'] = bios['link_id_bios'].str.replace('t3_', '')
+
+# merge both dataframes
+merged = pd.merge(posts, bios, left_on='link_id_posts', right_on='link_id_bios',
+                  how='outer', suffixes=('_posts', '_bios'))
+
+# Remove rows that are not actually bios
+mask = merged['body_bios'].str.contains("The OP has not provided a Bio for their post.") == False
 merged = merged[mask]
 
-merged.to_csv("combined.csv")
+# Write to csv
+merged.to_csv("bios_combined_with_posts_final.csv")
